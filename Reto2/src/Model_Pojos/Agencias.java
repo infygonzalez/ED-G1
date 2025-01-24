8481df8fbae3097b641f7132d4d29248211ce127
AlvaroBosque <alvaro.bosquepa@elorrieta-errekamari.com>
2025-01-24 09:50:02 +0100

    Creado el metodo constructor para Agencia

--- Reto2/src/Model_Pojos/Agencias.java
+++ Reto2/src/Model_Pojos/Agencias.java
@@ -1,5 +1,42 @@
-package Model_Pojos;
+package Modelo_Pojos;
 
 public class Agencias {
+ private String nombre;
+ private String color;
+ private String empleados;
+ private String tipo;
+ private String logo;
+
+ 
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getColor() {
+	return color;
+}
+public void setColor(String color) {
+	this.color = color;
+}
+public String getEmpleados() {
+	return empleados;
+}
+public void setEmpleados(String empleados) {
+	this.empleados = empleados;
+}
+public String getTipo() {
+	return tipo;
+}
+public void setTipo(String tipo) {
+	this.tipo = tipo;
+}
+public String getLogo() {
+	return logo;
+}
+public void setLogo(String logo) {
+	this.logo = logo;
+}
 
 }
