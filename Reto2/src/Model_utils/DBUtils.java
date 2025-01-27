13d7adee5251f1cecb1cf4f5dc74815cbe380672
MiguelGonz06 <miguel.gonzalezsa@elorrieta-errekamari.com>
2025-01-27 10:51:32 +0100

    Conexion DBUtils a BBDD

--- Reto2/src/Model_utils/DBUtils.java
+++ Reto2/src/Model_utils/DBUtils.java
@@ -1,8 +1,8 @@
 package Model_utils;
 
 public class DBUtils {
-	public static final String URL = "jdbc:mysql://localhost:3306/bdalumnos";
+	public static final String URL = "jdbc:mysql://localhost:3306/elorrietaviajes";
 	public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-	public static final String USER = "root";
-	public static final String PASS = "";
+	public static final String USER = "mañana";
+	public static final String PASS = "M@ñana!D4M3";
 }
